73ccb4f5fb612b71802ecb16987964b124c8e604
Killian KOPP <killiankopp@gmail.com>
2026-03-12 21:32:54 +0100

    feat: add ingredient management MCP tools
    This commit introduces a new IngredientMCP class that provides
    various tools for managing ingredients through a fastmcp interface.
    - Added methods to create, read, update, delete, and list ingredients.
    - Implemented functionality to duplicate ingredients and find them
      by name.
    This enhances the ingredient management capabilities of the system.

---
+++ infrastructure/mcp.py
@@ -0,0 +1,18 @@
+import fastmcp
+
+from adapters.input.ingredient_mcp import IngredientMCP
+from adapters.output.console_logger import ConsoleLogger
+from adapters.output.in_memory_ingredient_repository import InMemoryIngredientRepository
+from domain.services.ingredient_service import IngredientService
+
+
+def create_mcp() -> fastmcp.FastMCP:
+    mcp = fastmcp.FastMCP("Rekipe - Ingredients")
+
+    logger = ConsoleLogger()
+    ingredient_repository = InMemoryIngredientRepository()
+    ingredient_service = IngredientService(ingredient_repository, logger)
+    IngredientMCP(ingredient_service, logger, mcp)
+
+    return mcp
+
